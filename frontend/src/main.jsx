b5a1fb7b4bbd9ada0dca385fb1122181c1b5fa37
alperntsvrr <alperntsvr@gmail.com>
2026-01-03 01:16:10 +0300

    Sentry dsn

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@ import "./index.css";
 import * as Sentry from "@sentry/react";
 
 Sentry.init({
-  dsn:  import.meta.env.VITE_SENTRY_DSN, // .env file or environment variable
+  dsn: "https://412a3536d10c25c03817be2d62c753a7@o4510643121815552.ingest.de.sentry.io/4510643125813328",
   integrations: [
     Sentry.browserTracingIntegration(),
     Sentry.replayIntegration(),
